ff898092914fcb3606e7022a2c654815490bca5b
felex <felicxmose@msn.com>
2019-03-27 12:26:28 +0100

    tilføjet showAllCars function

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,23 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, { AxiosResponse, AxiosError } from "../../node_modules/axios/index"
+import { ICar } from "./Icar";
+
+let Contentelement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+
+function showAllCars():void{
+    axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
+    .then(function(response: AxiosResponse<ICar[]>): void
+    {
+        console.log(response);
+        //Contentelement.textContent = String(response.data[1]);
+    })
+    .catch(
+        function(error: AxiosError ): void{
+            console.log("errrrrrror in my code")
+            console.log(error);
+        }
+        
+    )   
+    console.log("er i slutning af af show all cars function");
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+showAllCars();
