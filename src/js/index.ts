581c029fc59f29427d27d467ad7808b2cfa90722
felex <felicxmose@msn.com>
2019-03-27 13:47:00 +0100

    implementing delete function

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,13 +2,59 @@ import axios, { AxiosResponse, AxiosError } from "../../node_modules/axios/index
 import { ICar } from "./Icar";
 
 let Contentelement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let getAllCarsBtn: HTMLButtonElement= <HTMLButtonElement>document.getElementById("getAllBtn");
+let getClearBtn:HTMLButtonElement = <HTMLButtonElement>document.getElementById("clearBtn");
+let addCarBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("addCarBtn");
+let deleteCarBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("deleteCarBtn");
+
+deleteCarBtn.addEventListener("click",deleteCar);
+
+function deleteCar():void{
+    
+}
+
+getAllCarsBtn.addEventListener("click", showAllCars);
+
+getClearBtn.addEventListener("click",() =>{
+
+    Contentelement.innerText = "";
+});
+
+addCarBtn.addEventListener("click", addCar);
+
+function addCar():void{
+    let addModelElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addModel");
+    let addVendorElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addVendor");
+    let addPriceElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addPrice");
+
+    let myModel: string = addModelElement.value;
+    let myVendor: string = addVendorElement.value;
+    let myPrice: number = Number(addPriceElement.value);
+    
+    axios.post<ICar>("https://webapicar20190326034339.azurewebsites.net/api/cars",
+                    {model:myModel, vendor:myVendor,price:myPrice})
+        .then(function (response:AxiosResponse):void{
+            console.log("status koden er:" + response.status)    
+        })
+        .catch(function(error: AxiosError):void{
+
+        })
+}
 
 function showAllCars():void{
     axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
     .then(function(response: AxiosResponse<ICar[]>): void
     {
         console.log(response);
-        //Contentelement.textContent = String(response.data[1]);
+
+        let result: string = "<ol>"
+        
+        response.data.forEach((car: ICar) => {
+            result += "<li>"+car.model +" "+car.vendor+" "+car.id +"</li>"    
+        });
+        result +="</ol>"
+
+        Contentelement.innerHTML = result;
     })
     .catch(
         function(error: AxiosError ): void{
@@ -20,4 +66,4 @@ function showAllCars():void{
     console.log("er i slutning af af show all cars function");
 }
 
-showAllCars();
+//showAllCars();
